cdc0228d7483f563133e9d8674dd687c4071795d
Sunny <sunnybaby28@gmail.com>
2023-12-30 18:52:37 +0800

    speaker verification by lstm and mfcc

--- evaluation.py
+++ evaluation.py
@@ -25,21 +25,20 @@ def run_inference(features, encoder, full_sequence=myconfig.USE_FULL_SEQUENCE_IN
         aggregated_output = torch.mean(batch_output, dim=0, keepdim=False).cpu()
         return aggregated_output.data.numpy()
 
+
 def cosine_similarity(a, b):
     """Compute cosine similarity between two embeddings."""
     return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
 
 def compare_two(file1, file2):
     """Run evaluation of the saved model on test data."""
     start_time = time.time()
 
-    encoder = neural_net.get_speaker_encoder(r'./saved_model/saved_model_20230514232827.pt')
-    # labels, scores = compute_scores(encoder, spk_to_utts, myconfig.NUM_EVAL_TRIPLETS)
+    encoder = neural_net.get_speaker_encoder(r'./saved_model/saved_model_lstm.pt')
 
     features1 = feature_extraction.extract_features(file1)
     features2 = feature_extraction.extract_features(file2)
-    # print(features1)
-    # print(features2)
     
     embedding1 = run_inference(features1, encoder)
     embedding2 = run_inference(features2, encoder)
@@ -47,13 +46,13 @@ def compare_two(file1, file2):
     scores = cosine_similarity(embedding1, embedding2)
 
     eval_time = time.time() - start_time
-    print("Finished evaluation in", scores, "seconds")
+    print("Finished evaluation in", eval_time, "seconds")
+    print("Finished scores", scores)
 
     if scores >= 0.8:
         return True
 
     return False
-
 
 
 if __name__ == "__main__":
